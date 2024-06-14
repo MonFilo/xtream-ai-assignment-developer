ebb95fdb1681b4a3c471873c74e7bd28e879dd02
MonFilo <filippomonaco@outlook.com>
2024-06-14 10:28:33 +0200

    added train test split to data loading

--- challenge/data/_load_data.py
+++ challenge/data/_load_data.py
@@ -1,25 +1,54 @@
 import pandas as pd
-import pathlib as Path
+from pathlib import Path
 from sklearn.model_selection import train_test_split
 
 def load_data(file_path:Path):
     """
     Reads the data in file_path and returns is as pandas dataframe
+
+    Parameters:
+        - file_path (pathlib.Path): file path to diamonds.csv
+
+    Returns:
+        - dataframe (X):    dataframe of covariates
+        - dataframe (y):    single-column dataframe of response
     """
+
+    # assert that file has correct extension
     assert file_path.suffix in ['.csv', '.txt']
+
+    # read file into pandas dataframe
     data = pd.read_csv(file_path, header = 0)
-    print(data.head())
+
+    # load covariates
     X = data.drop(columns = 'price', axis = 1)
+
+    # load response
     y = data['price']
+
     return X, y
 
-def split(dataframe:pd.DataFrame):
+def split(X:pd.DataFrame, y:pd.DataFrame, **kwargs):
     """
+    Split input dataframe into train, validation, and testing dataframes
+
+    Parameters:
+        - X (pandas.DataFrame): input dataframe of covariates to split
+        - y (pandas.DataFrame): input dataframe of response to split
+        - **kwargs: addittional named arguments for sl;earm train_test_split function
+
+    Returns:
+        - dataframe (X_train):    dataframe of covariates for training
+        - dataframe (X_test):    dataframe of covariates for testing
+        - dataframe (y_train):    dataframe of response for training
+        - dataframe (y_test):    dataframe of response for testing
     """
-    df_train, df_val, df_test = train_test_split()
+    return train_test_split(X, y, **kwargs)
 
 if __name__ == '__main__':
-    current_dir = Path.PurePath(__file__).parent
+    current_dir = Path(__file__).parent
     parent_dir = current_dir.parents[1]
     csv_file_path = parent_dir / 'data' / 'diamonds.csv'  # Construct the path to the CSV file
     X, y = load_data(csv_file_path)
+    X_train, X_test, y_train, y_test = split(X, y, train_size = 0.8, random_state = 48)
+    print(y_test)
